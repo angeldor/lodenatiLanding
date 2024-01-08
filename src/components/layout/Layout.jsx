70c0a3e60635faf6a6bc3d73541862a9006b1978
AngelBarrios <angelefrenbarriosromero234@gmail.com>
2024-01-08 20:26:05 -0300

    adding layout

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -1,10 +1,21 @@
 import React from "react";
 import Navbar from "./navbar/Navbar";
+import {Outlet} from "react-router-dom"
+import Footer from "./footer/Footer";
 
 const Layout = () => {
   return (
-    <Navbar/>
-    
+    <div>
+      <div style={{height: "20vh"}}>
+        <Navbar/>
+      </div>
+      <div style={{ minHeight: "70vh" }}>
+        <Outlet/>
+      </div>
+      <div style={{ height: "10vh" }}>
+        <Footer/>
+      </div>
+    </div>
   )
 };
 
